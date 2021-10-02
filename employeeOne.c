ef7c930952d28a29094e3c567f9cbf67c37a88b7
Alexa Tang <46989357+AlexaTTang@users.noreply.github.com>
2021-10-01 22:21:14 -0700

    phone and salary search

--- employeeOne.c
+++ employeeOne.c
@@ -28,5 +28,32 @@ PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char *
         }
     }
     return NULL; // will only happen if no Employee number matches in loop above
+}
 
+PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int tableSize, char * targetPhone)
+{
+    const PtrToConstEmployee endPtr = ptr + tableSize;
+                        //next "row" so think if int ptr, ptr++ would increment 4 bytes
+    for(; ptr < endPtr;     ptr++   )// search until end of the table
+    {       // to number
+        if (strcmp(ptr -> phone, targetPhone) == 0)//check if it equals the employee phone number
+        {
+            return(PtrToEmployee) ptr; //return the location of the number to callee
+        }
+    }
+    return NULL; // will only happen if no Employee phone matches in loop above
+}
+
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr,           int tableSize, double targetSalary)
+{
+    //figuring out the end of the table, we know addr of the beginning of the table
+    const PtrToConstEmployee endPtr = ptr + tableSize; // pointing to the end of the table, boundary
+    for(; ptr < endPtr; ptr++)// search until end of the table
+    {       // to number
+        if (ptr -> salary == targetSalary)//check if it equals the employee number
+        {
+            return(PtrToEmployee) ptr; //return the location of the number to callee
+        }
+    }
+    return NULL; // will only happen if no Employee number matches in loop above
 }
